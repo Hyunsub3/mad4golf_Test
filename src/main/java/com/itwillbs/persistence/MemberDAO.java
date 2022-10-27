e3f1e1eedfc8d717466d6bb4264027241e01859e
0412shsh <0412corn@naver.com>
2022-10-27 13:02:18 +0900

    MemberJoin - MemberJoin - 회원가입
    insert
    
    insert.jsp
    MemberController.java
    MemberDAO.java
    MemberDAOImpl.java
    MemberMapper.xml
    MemberService.java
    MemberServiceImpl.java
    MemberVO.java

---
+++ src/main/java/com/itwillbs/persistence/MemberDAO.java
@@ -0,0 +1,12 @@
+package com.itwillbs.persistence;
+
+import com.itwillbs.domain.MemberVO;
+
+public interface MemberDAO {
+
+	//회원가입 처리
+	public void insert(MemberVO vo) throws Exception;
+	
+	//아이디 중복 체크 
+	public int idCheck(MemberVO vo) throws Exception;
+}

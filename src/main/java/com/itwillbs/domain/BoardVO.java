26792f956dd534d60df54b9e87a9f793dac9aaba
Dev_Aaron <hyunsub3@gmail.com>
2022-10-27 12:18:06 +0900

    build version

--- src/main/java/com/itwillbs/domain/BoardVO.java
+++ src/main/java/com/itwillbs/domain/BoardVO.java
@@ -20,6 +20,48 @@ public class BoardVO {
 	private String writer;
 	private int viewcnt;
 	private Timestamp regdate;
+	public Integer getBno() {
+		return bno;
+	}
+	public void setBno(Integer bno) {
+		this.bno = bno;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public int getViewcnt() {
+		return viewcnt;
+	}
+	public void setViewcnt(int viewcnt) {
+		this.viewcnt = viewcnt;
+	}
+	public Timestamp getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Timestamp regdate) {
+		this.regdate = regdate;
+	}
+	
+	@Override
+	public String toString() {
+		return "BoardVO [bno=" + bno + ", title=" + title + ", content=" + content + ", writer=" + writer + ", viewcnt="
+				+ viewcnt + ", regdate=" + regdate + "]";
+	}
 	
 	
 }
